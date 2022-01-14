91dfc1e000513487f159b52c76bfd1a317edfc47
Edwin Luo <edwin.luo@certik.org>
2022-01-13 20:19:08 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000/");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    socket.on("connection", () => {
+      console.log(`I'm connected with the back-end`);
+    });
+    socket.on("new", () => {
+      console.log("client event new triggered");
+    });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
